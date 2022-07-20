d099cdaea3338b47b0405b9bdd2e5961a53f5010
Alwayfeels <547542652@qq.com>
2022-07-20 17:04:24 +0800

    fix bug

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,4 +3,8 @@ module.exports = {
   dest: 'public',
   title: 'Alwayfeels\'s Blog',
   description: 'Just a place to share my thoughts',
+  themeConfig: {
+    sidebar: 'auto',
+    displayAllHeaders: true // 默认值：false
+  }
 }
